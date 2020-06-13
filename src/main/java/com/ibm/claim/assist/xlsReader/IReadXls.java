6d694997f432f51759c6d1a970e3450ab89f7e25
SantoshSGhatage <santu.ghatge@gmail.com>
2020-06-13 17:37:29 +0200

    Checking Git Commit from intellij

--- src/main/java/com/ibm/claim/assist/xlsReader/IReadXls.java
+++ src/main/java/com/ibm/claim/assist/xlsReader/IReadXls.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public interface IReadXls {
 
+    //Sngat received
     public List<EmpContact> getEmpContact();
     public List<ReadAccountClaimData> getAccountClaims();
 
